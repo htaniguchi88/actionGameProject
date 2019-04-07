12deab74f4fac226d52cfd6570f2915254998059
htaniguchi88 <gucci.taniguchi88@gmail.com>
2019-04-07 12:56:15 +0900

    Add assets

--- Assets/Scripts/OnCollisionDestroyer.cs
+++ Assets/Scripts/OnCollisionDestroyer.cs
@@ -3,7 +3,7 @@
 public class OnCollisionDestroyer : MonoBehaviour {
     void OnTriggerEnter2D(Collider2D coll)
     {
-        Destroy(coll.gameObject);
         Destroy(gameObject);
+        if (coll.gameObject.tag != "Border") Destroy(coll.gameObject);
     }
 }
--- Assets/Scripts/VerticalMovement.cs
+++ Assets/Scripts/VerticalMovement.cs
@@ -2,9 +2,14 @@
 
 public class VerticalMovement : MonoBehaviour
 {
+
+    void Start() {
+        Vector3 tmp = GameObject.Find("Player").transform.position;
+        transform.position = new Vector3(tmp.x, -3, tmp.z);
+    }
+
     void Update()
     {
         transform.Translate(0, 0.18f, 0);
-        if (transform.position.y > 4) Destroy(gameObject);
     }
 }
